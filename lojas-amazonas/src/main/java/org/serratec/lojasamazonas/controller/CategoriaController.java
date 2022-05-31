7be3963449f5eeabb29904774c959b26901a51ec
Sarah Almeida <sarahalmeida013@gmail.com>
2022-05-31 12:42:23 -0300

    Criação endpoints (create e get) da entidade Categoria

---
+++ lojas-amazonas/src/main/java/org/serratec/lojasamazonas/controller/CategoriaController.java
@@ -0,0 +1,39 @@
+package org.serratec.lojasamazonas.controller;
+
+import java.util.List;
+
+import org.serratec.lojasamazonas.dto.CategoriaDTO;
+import org.serratec.lojasamazonas.dto.CategoriaDTORequest;
+import org.serratec.lojasamazonas.exception.ItemNotFoundException;
+import org.serratec.lojasamazonas.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+	
+	@Autowired
+	CategoriaService service;
+	
+	@PostMapping
+	public ResponseEntity<String> create(@RequestBody CategoriaDTORequest categoria) {
+		return ResponseEntity.ok(service.crate(categoria));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> getAll() {
+		return ResponseEntity.ok(service.getAll());
+	}
+	
+	@GetMapping("/{codigoCategoria}")
+	public ResponseEntity<CategoriaDTO> getByCodigol(@PathVariable Long codigoCategoria) throws ItemNotFoundException {
+		return ResponseEntity.ok(service.getByCodigo(codigoCategoria));
+	}
+}
